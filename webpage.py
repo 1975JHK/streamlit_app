9119175eb958509a1c39bb7b2df4a92fcd354012
1975JHK <79017517+1975JHK@users.noreply.github.com>
2024-03-10 23:10:33 +0900

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib
-import matplotlib import font_manager
+from matplotlib import font_manager
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
